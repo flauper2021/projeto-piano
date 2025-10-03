653a6427f7876eacd9ada23f647add75754854ff
mauricioarrigada <mauricio.work@yahoo.com>
2025-10-03 10:07:16 -0300

    atribuindo sons ao teclado

---
+++ src/scripts/engine.js
@@ -0,0 +1,14 @@
+const pianoKeys = document.querySelectorAll('.piano-keys .keys');
+
+let audio = new Audio("./src/tunes/a.wav");
+
+const playTune = (key) => {
+    audio.src = `./src/tunes/${key}.wav`;
+    audio.play();
+};
+
+
+pianoKeys.forEach(key => {
+    console.log(key.dataset.key);
+    key.addEventListener("click", () => playTune(key.dataset.key));
+})
